e110753f8194cb017c99d95289c3fcd32edae420
Gaurav Khunt <gaurav.khunt@emperorbrains.com>
2023-01-07 13:58:01 +0530

    ADD Function Add , Edit, Delate

--- src/components/student/List.vue
+++ src/components/student/List.vue
@@ -5,11 +5,26 @@ import {
   TrashIcon,
   UserPlusIcon,
 } from "@heroicons/vue/24/solid";
+import { onMounted } from "vue";
+import { RouterLink } from "vue-router";
+import useStudent from "../../composables/studentApi";
+
+const {
+  studentData,
+  error,
+  statusCode,
+  delError,
+  getAllStudent,
+  destroyStudent,
+} = useStudent();
+onMounted(getAllStudent);
+
 const deleteStudent = async (id) => {
   if (!window.confirm("Are you sure ?")) {
     return;
   }
-  console.log("delate");
+  await destroyStudent(id);
+  await getAllStudent();
 };
 </script>
 
@@ -22,16 +37,23 @@ const deleteStudent = async (id) => {
         </h1>
       </div>
       <div class="text-right">
-        <RouterLink :to="{ name: 'ADD' }">
+        <RouterLink :to="{ name: 'add' }">
           <button
             class="text-white text-md bg-green-700 hover:bg-green-800 font-medium rounded-lg p-2 px-6"
           >
-            <UserPlusIcon></UserPlusIcon> Add
-          </button></RouterLink
-        >
+            <UserPlusIcon /> Add
+          </button>
+        </RouterLink>
       </div>
     </div>
-    <table class="table-auto w-full">
+    <div
+      class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-medium"
+      role="alert"
+      v-if="error"
+    >
+      Oops! Error encountered: {{ error.message }}
+    </div>
+    <table class="table-auto w-full" v-else-if="studentData">
       <thead class="bg-slate-600 text-white">
         <tr>
           <th class="py-1">No</th>
@@ -41,28 +63,39 @@ const deleteStudent = async (id) => {
         </tr>
       </thead>
       <tbody class="text-center">
-        <tr>
-          <td class="py-2">1</td>
-          <td class="py-2">Gaurav</td>
-          <td class="py-2">gauravpatel11@gmail.com</td>
+        <tr v-for="({ id, stuname, email }, i) in studentData" :key="id">
+          <td class="py-2">{{ ++i }}</td>
+          <td class="py-2">{{ stuname }}</td>
+          <td class="py-2">{{ email }}</td>
           <td class="py-2">
-            <RouterLink :to="{ name: 'View', params: { id: 1 } }">
-              <EyeIcon class="text-blue-500 h-6 w-6 inline"></EyeIcon
-            ></RouterLink>
-            <RouterLink :to="{ name: 'Edit', params: { id: 1 } }">
-              <PencilIcon
-                class="text-emerald-500 h-6 w-6 mx-6 inline"
-              ></PencilIcon
-            ></RouterLink>
-
+            <RouterLink :to="{ name: 'view', params: { id: id } }">
+              <EyeIcon class="text-blue-500 h-6 w-6 inline" />
+            </RouterLink>
+            <RouterLink :to="{ name: 'edit', params: { id: id } }">
+              <PencilIcon class="text-emerald-500 h-6 w-6 inline mx-6" />
+            </RouterLink>
             <TrashIcon
               class="text-red-500 h-6 w-6 inline cursor-pointer"
-              @click="deleteStudent(1)"
-            ></TrashIcon>
+              @click="deleteStudent(id)"
+            />
           </td>
         </tr>
       </tbody>
     </table>
+    <div
+      class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-medium"
+      role="alert"
+      v-if="delError"
+    >
+      Unable to Delete Student: {{ delError.message }}
+    </div>
+    <div
+      class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-medium"
+      role="alert"
+      v-if="statusCode === 200"
+    >
+      Student Deleted Successfully
+    </div>
   </div>
 </template>
 
--- src/components/student/View.vue
+++ src/components/student/View.vue
@@ -1,9 +1,25 @@
-<script setup></script>
+<script setup>
+import useStudent from "../../composables/studentApi";
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+const { studentData, error, getSingleStudent } = useStudent();
+const route = useRoute();
+onMounted(() => {
+  getSingleStudent(route.params.id);
+});
+</script>
 
 <template>
   <div class="shadow-md pb-6">
-    <table class="table-auto w-full">
-      <thead class="bg-slate-600 text-white w-full">
+    <div
+      class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg font-medium"
+      role="alert"
+      v-if="error"
+    >
+      Oops! Error encountered: {{ error.message }}
+    </div>
+    <table class="table-auto w-full" v-else-if="studentData">
+      <thead class="bg-slate-600 text-white">
         <tr>
           <th class="py-1">ID</th>
           <th class="py-1">Name</th>
@@ -12,21 +28,21 @@
       </thead>
       <tbody class="text-center">
         <tr>
-          <td class="py-2">1</td>
-          <td class="py-2">Gaurav</td>
-          <td class="py-2">gauravpatel11@gmail.com</td>
+          <td class="py-2">{{ studentData.id }}</td>
+          <td class="py-2">{{ studentData.stuname }}</td>
+          <td class="py-2">{{ studentData.email }}</td>
         </tr>
       </tbody>
     </table>
-    <div class="text-center">
-      <RouterLink :to="{ name: 'List' }">
+    <div class="m-8 flex justify-center">
+      <RouterLink :to="{ name: 'list' }">
         <button
           type="button"
-          class="bg-emerald-700 text-white font-medium py-2 rounded-md px-6 hover:bg-emerald-800 mr-6"
+          class="bg-emerald-700 text-white font-medium p-2 rounded-md hover:bg-emerald-800"
         >
           Back to Home
-        </button></RouterLink
-      >
+        </button>
+      </RouterLink>
     </div>
   </div>
 </template>
